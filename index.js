caec7e6c952fddbb48cd1399461c0feb6d2e87f2
WarriorGeek25 <22shannon.rufus@gmail.com>
2022-01-15 16:19:04 +0000

    1 file modified, 4 added, 5 deleted

--- index.js
+++ index.js
@@ -2,18 +2,14 @@
 import './style.css';
 
 // Write Javascript code!
-var canvas = document.getElementById('myCanvas');
+var canvas = document.getElementById('MyCanvas');
 var ctx = canvas.getContext('2d');
 
-
 ctx.fillStyle = 'green';
-ctx.fillRect(95, 10, 150, 150);
+ctx.fillRect(95, 10, 170, 150);
 
 ctx.beginPath();
-ctx.arc(170, 80, 30, 0, 2* Math.PI);
+ctx.arc(178, 80, 30, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.fillStyle = 'red';
 ctx.fill();
-
-
-
